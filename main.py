ba01a599a96415d76503a10939c8215c240c5b56
Elena Navarro <elena.navarro@uclm.es>
2025-11-25 11:08:30 +0100

    creamos la api con api key y generacion de spo2 y temperatura

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Request, Depends
+from fastapi.security import APIKeyHeader
+from starlette.status import HTTP_401_UNAUTHORIZED
 from pydantic import BaseModel
 from azure.storage.blob import BlobServiceClient
 import pandas as pd
@@ -7,13 +9,25 @@ from datetime import datetime
 import io
 import time
 import os
+import math
 
 # Configuración Azure como variables de entorno
 AZURE_CONNECTION_STRING = os.getenv("AZURE_CONNECTION_STRING") 
 CONTAINER_NAME =  os.getenv("AZURE_CONTAINER_NAME") 
-#CONTAINER_NAME =  "contenedor-demo"
 
-app = FastAPI()
+# Clave de API autorizada definida como variable de entorno
+API_KEY_AUTORIZADA = os.getenv("API_KEY") 
+
+# Definición del tipo de seguridad con el nombre de la cabecera
+api_key_header = APIKeyHeader(name="X-API-Key", auto_error=False)
+
+# Función de dependencia para verificar la clave de API
+async def verificar_api_key(api_key_param: str = Depends(api_key_header)):
+    if api_key_param != API_KEY_AUTORIZADA:
+        raise HTTPException(status_code=401, detail="API Key no valida verifica la variable de entorno")
+    return True
+
+app = FastAPI(title="API para generación de archivos CSV con Protección de API Key")
 
 class UploadRequest(BaseModel):
     folder_name: str       # Carpeta principal
@@ -23,22 +37,45 @@ class UploadRequest(BaseModel):
     duration: int          # Tiempo total (segundos)
     id_max_paciente: int   # numero max de id de pacientes que tenemos
 
-@app.post("/start-upload")
+@app.post("/start-upload", dependencies=[Depends(verificar_api_key)])
 def start_upload(request: UploadRequest):
+    # creamos el cliente
     blob_service_client = BlobServiceClient.from_connection_string(AZURE_CONNECTION_STRING)
-    num_files = request.duration // (request.latency *1000)
+    
+    #asignamos por defecto cada 1000 ms
+    request.latency=1000 if request.latency==0 else request.latency
+
+    print(request.latency )
+
+    #asignamos por defecto que la duración son 1 segundos
+    request.duration=1 if request.duration==0 else request.duration
+
+    #asignamos por defecto que el número máximo de id de paciente es 108
+    request.id_max_paciente=8 if request.id_max_paciente==0 else request.id_max_paciente
+
+    #asignamos por defecto número de filas
+    request.rows=2 if request.rows==0 else request.rows
+
+
+    #asignamos por defecto carpeta donde se guardara
+    request.folder_name="spo2-temperatura" if request.folder_name is None else request.folder_name
+    
+    num_files = math.trunc(request.duration / (request.latency /1000))
+    
     uploaded_files = []
 
     for i in range(num_files):
         # Generar datos aleatorios
         data = []
-        id_max_paciente= 100 +request.rows
-        for _ in range(request.rows):
-            id_paciente = math.trunc(random.uniform(101, id_max_paciente))
-            if generar_error_temperatura > 90:
-  	         temperatura = round(random.uniform(-100, 100), 2)
-            else:
-             temperatura = round(random.uniform(20, 50), 2)
+        id_paciente = math.trunc(random.uniform(101, request.id_max_paciente))
+
+        # generamos aleatoriamente errores de temperatura para comprobar la validacion de datos
+        generar_error_temperatura=random.uniform(0, 100)
+        if generar_error_temperatura > 90:
+            temperatura = round(random.uniform(-100, 100), 2)
+        else:
+            temperatura = round(random.uniform(20, 50), 2)
+
             SpO2 = round(random.uniform(60, 100), 2)
             timestamp = datetime.utcnow().isoformat()
             data.append({"id_paciente":id_paciente, "temperatura": temperatura, "SpO2": SpO2, "timestamp": timestamp })
